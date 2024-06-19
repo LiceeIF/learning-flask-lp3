06d2adba83f9acad3fc65885583208f1fdc8c52f
LiceeIF <alice.maria@aluno.ifsp.edu.br>
2024-06-19 15:12:44 -0300

    updating methods

--- alice/app.py
+++ alice/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 lista_produtos = [
-    { "nome": "Coca-cola", "descricao": "bom"},
-    { "nome": "Doritos", "descricao": "suja mao"},
-    { "nome": "Chocolate", "descricao": "bom"},
+    { "nome": "Coca-cola", "descricao": "bom", "preco": "5.99", "imagem": "https://images.tcdn.com.br/img/img_prod/948333/coca_cola_290ml_c24_769_1_eac646cd43dbe5f0b415423f9887f599.png"},
+    { "nome": "Doritos", "descricao": "suja mao", "preco": "8,99", "imagem": "https://atacadaobr.vtexassets.com/arquivos/ids/252509/g.jpg?v=638353972719200000"},
+    { "nome": "Chocolate", "descricao": "bom", "preco": "10,99", "imagem": "https://images.tcdn.com.br/img/img_prod/907279/barra_de_chocolate_ao_leite_oreo_100g_8629_1_1bba077622edae3b68daebf755ca4443.jpg"},
 ]
 
 app = Flask(__name__)
@@ -30,3 +30,23 @@ def produto(nome):
             return render_template("produto.html", produto=produto)
         
     return "produto não encontrado"
+
+#GET
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro-produto.html")
+
+#POST
+@app.route("/produtos", methods = ["POST"])
+def salvar_produto():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    preco = request.form['preco']
+    imagem = request.form['imagem']
+    produto = {"nome": nome, "descricao": descricao, "preco": preco, "imagem": imagem}
+    lista_produtos.append(produto)
+
+    return redirect(url_for("produtos"))
+
+
+app.run(port=5001)
