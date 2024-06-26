68252377640df9bb65526cd6cf7a0ada78444961
LiceeIF <alice.maria@aluno.ifsp.edu.br>
2024-06-26 15:11:08 -0300

    updating class

--- alice/app.py
+++ alice/app.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+from validate_docbr import CPF, CNPJ
 
-lista_produtos = [
-    { "nome": "Coca-cola", "descricao": "bom", "preco": "5.99", "imagem": "https://images.tcdn.com.br/img/img_prod/948333/coca_cola_290ml_c24_769_1_eac646cd43dbe5f0b415423f9887f599.png"},
-    { "nome": "Doritos", "descricao": "suja mao", "preco": "8,99", "imagem": "https://atacadaobr.vtexassets.com/arquivos/ids/252509/g.jpg?v=638353972719200000"},
-    { "nome": "Chocolate", "descricao": "bom", "preco": "10,99", "imagem": "https://images.tcdn.com.br/img/img_prod/907279/barra_de_chocolate_ao_leite_oreo_100g_8629_1_1bba077622edae3b68daebf755ca4443.jpg"},
-]
+def obter_produtos():
+    # 1. Abrir o arquivo
+    with open("produtos.csv", "r") as file:
+        lista_produtos = []
+        # 2. Ler o arquivo (cada linha)
+        for linha in file:
+            # 3. Quebrar a linha na vírgula ['Coca-cola', 'dsadas', '10', 'coca.jpg']
+            nome, descricao, preco, imagem = linha.strip().split(",")
+            # 4. Montar o dicionário com os valores da lista
+            produto = {
+                "nome": nome,
+                "descricao": descricao,
+                "preco": float(preco),
+                "imagem": imagem
+            }
+            lista_produtos.append(produto)
+        return lista_produtos
+    
+def adicionar_produto(produto):
+    linha = f"\n{produto['nome']}, {produto['descricao']}, {produto['preco']}, {produto['imagem']}"
+    with open("produtos.csv", 'a') as file:
+        file.write(linha)
+        
+
+# lista_produtos = [
+#     { "nome": "Coca-cola", "descricao": "bom", "preco": "5.99", "imagem": "https://images.tcdn.com.br/img/img_prod/948333/coca_cola_290ml_c24_769_1_eac646cd43dbe5f0b415423f9887f599.png"},
+#     { "nome": "Doritos", "descricao": "suja mao", "preco": "8,99", "imagem": "https://atacadaobr.vtexassets.com/arquivos/ids/252509/g.jpg?v=638353972719200000"},
+#     { "nome": "Chocolate", "descricao": "bom", "preco": "10,99", "imagem": "https://images.tcdn.com.br/img/img_prod/907279/barra_de_chocolate_ao_leite_oreo_100g_8629_1_1bba077622edae3b68daebf755ca4443.jpg"},
+# ]
 
 app = Flask(__name__)
 
@@ -21,11 +46,11 @@ def contato():
 
 @app.route("/produtos")
 def produtos():
-    return render_template('produtos.html', produtos=lista_produtos)
+    return render_template('produtos.html', produtos=obter_produtos())
 
 @app.route("/produtos/<nome>")
 def produto(nome):
-    for produto in lista_produtos:
+    for produto in obter_produtos():
         if produto["nome"].lower() == nome.lower():
             return render_template("produto.html", produto=produto)
         
@@ -44,9 +69,20 @@ def salvar_produto():
     preco = request.form['preco']
     imagem = request.form['imagem']
     produto = {"nome": nome, "descricao": descricao, "preco": preco, "imagem": imagem}
-    lista_produtos.append(produto)
+    adicionar_produto(produto)
 
     return redirect(url_for("produtos"))
 
+@app.route("/gerarcpf")
+def gerar_cpf():
+    cpf = CPF()
+    new_cpf = cpf.generate()
+    return render_template('gerar-cpf.html', show_cpf=new_cpf)
+
+@app.route("/gerarcnpj")
+def gerar_cnpj():
+    cnpj = CNPJ()
+    new_cnpj = cnpj.generate()
+    return render_template('gerar-cnpj.html', show_cnpj=new_cnpj)
 
 app.run(port=5001)
